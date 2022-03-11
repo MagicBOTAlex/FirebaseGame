454fffae5499b65dd229824f55e9ef9ce03f8ede
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-03-11 13:23:53 +0100

    Added add score to firebase handler

--- Assets/Scripts/FirebaseHandler.cs
+++ Assets/Scripts/FirebaseHandler.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Firebase;
 using Firebase.Database;
 using Firebase.Database.Http;
+using Newtonsoft.Json;
 
 public static class FirebaseHandler 
 {
@@ -11,6 +15,38 @@ public static class FirebaseHandler
 
     public static void PersonalMain()
     {
-        firebase.Child("database").PatchAsync("{\"testing\":true}").Wait();
+        var jObject = new DatabaseStructure();
+        var random = new Random();
+        var randomArr = new int[10];
+        for (int i = 0; i < randomArr.Length; i++)
+        {
+            randomArr[i] = random.Next(1000);
+        }
+        jObject.Scores = randomArr;
+
+        firebase.Child("database").PatchAsync(JsonConvert.SerializeObject(jObject)).Wait();
     }
+
+    public static DatabaseStructure GetData()
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync("https://fir-unity-9-3-2022-default-rtdb.europe-west1.firebasedatabase.app/database.json");
+
+        return JsonConvert.DeserializeObject<DatabaseStructure>(response.ToString());
+    }
+
+    public static void AddScore(int score)
+    {
+        var data = GetData();
+        var dumbList = data.Scores.ToList();
+        dumbList.Add(score);
+        data.Scores = dumbList.ToArray();
+
+        firebase.Child("database").PatchAsync(JsonConvert.SerializeObject(data)).Wait();
+    }
+}
+
+public class DatabaseStructure
+{
+    public int[] Scores { get; set; }
 }
