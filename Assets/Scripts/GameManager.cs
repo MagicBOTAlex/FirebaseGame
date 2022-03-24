029f3b98cfa8655bd1374e5069815b203af9c861
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-03-24 13:46:50 +0100

    Remade the whole platform spawning system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     public float PlayerModeSpeed = 50;
     public float MaxForwardSpeed = 20;
     public float MaxSideSpeed = 20;
+    public int PreSpawnedPlatforms = 3;
 
     [Header("Game values")]
     public float CurrentSpeed = 50;
@@ -28,10 +29,15 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this);
         }
+
+        for (int i = 0; i < PreSpawnedPlatforms; i++)
+        {
+            SpawnNewPlatform();
+        }
     }
 
     public void SpawnNewPlatform()
     {
-        SpawnedPlatforms.Add(Instantiate(Slopes[Random.Range(0, Slopes.Length)], Player.transform.GetChild(0).position, Quaternion.identity));
+        SpawnedPlatforms.Add(Instantiate(Slopes[Random.Range(0, Slopes.Length)], SpawnedPlatforms[SpawnedPlatforms.Count - 1].GetComponent<SlopeScript>().SpawnNextHere.transform.position, Quaternion.identity));
     }
 }
