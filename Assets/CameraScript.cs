e71b2e7b333fe8ae7cad9711706c7b1c439dcd24
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-04-20 13:38:24 +0200

    hidaghvdh9wji

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -6,15 +6,18 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject Player;
 
-    Rigidbody rb;
+    [Range(0, 1)]
+    public float Weight = 1.0f;
+
+    Transform tf;
 
     private void Start()
     {
-        rb = Player.GetComponent<Rigidbody>();
+        tf = Player.transform;
     }
 
     void Update()
     {
-        transform.position = Player.transform.position + Vector3.RotateTowards(transform.position - Player.transform.position, -Player.GetComponent<Rigidbody>().velocity.normalized * 2, Time.deltaTime, .0f);
+        transform.position = new Vector3(tf.position.x * Weight, tf.position.y, tf.position.z);
     }
 }
